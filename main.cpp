448cccb28aa3321f4b2e6db7396f63825917a20f
Jan Navratil <x.jan.navratil@gmail.com>
2012-10-19 22:51:31 +0200

    added print iteration

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@ int main()
 {
   cout << "Starting" << endl;
 	for( int i=0; i<3; ++i )
+  {
+    cout << i << endl;
 		cout << "Hello world" << endl;
+  }
   cout << "The End" << endl;
 
 	return 0;
